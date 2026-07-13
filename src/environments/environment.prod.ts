1ad00021d0ba21444086f64530f1086635caef97
camilo perez <perezvelasquezcamilo@gmail.com>
2026-07-12 21:17:40 -0500

    nu

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,18 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-  firebase: {  },
-  production: false,
+  production: true,
 
+
+  authApi: 'https://sigiv2.onrender.com/auth',
+  usersApi: 'https://sigiv2.onrender.com/usuarios',
+  empresasApi: 'https://sigiv2.onrender.com/empresas',
+  proveedoresApi: 'https://sigiv2.onrender.com/proveedores',
+  categoriasApi: 'https://sigiv2.onrender.com/categorias',
+  productosApi: 'https://sigiv2.onrender.com/productos',
+  ventasApi: 'https://sigiv2.onrender.com/ventas',
+  cotizacionesApi: 'https://sigiv2.onrender.com/cotizaciones',
+  detalleCotizacionApi: 'https://sigiv2.onrender.com/api/detallecotizacion',
+  devolucionesApi: 'https://sigiv2.onrender.com/api/devoluciones',
+  personasApi: 'https://sigiv2.onrender.com/personas',
+  nominasApi: 'https://sigiv2.onrender.com/nominas',
+  personaNominaApi: 'https://sigiv2.onrender.com/persona-nomina',
 };
-
